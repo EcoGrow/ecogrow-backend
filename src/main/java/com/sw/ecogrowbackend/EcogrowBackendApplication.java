32cf609f52a820994851062593f6d157a8315976
gminnimk <tvt1245@naver.com>
2024-09-09 17:10:12 +0900

    init commit

---
+++ src/main/java/com/sw/ecogrowbackend/EcogrowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sw.ecogrowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcogrowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcogrowBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sw/ecogrowbackend/EcogrowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.ecogrowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcogrowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
