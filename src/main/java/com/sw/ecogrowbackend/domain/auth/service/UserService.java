7f60a475b97b56c371d83d673ef9fde7f12746e7
gminnimk <tvt1245@naver.com>
2024-09-24 17:27:59 +0900

    fix : 기존 adminToken 삭제

--- src/main/java/com/sw/ecogrowbackend/domain/auth/service/UserService.java
+++ src/main/java/com/sw/ecogrowbackend/domain/auth/service/UserService.java
@@ -27,8 +27,6 @@ public class UserService {
     private final RefreshTokenService refreshTokenService;
     private final JwtUtil jwtUtil;
 
-    private final String ADMIN_TOKEN = "";
-
     /**
      * 회원가입 로직
      *
@@ -55,14 +53,7 @@ public class UserService {
             throw new IllegalArgumentException("중복된 Email 입니다.");
         }
 
-        // 사용자 ROLE 확인
         UserRoleEnum role = UserRoleEnum.USER;
-        if (requestDto.isAdmin()) {
-            if (!ADMIN_TOKEN.equals(requestDto.getAdminToken())) {
-                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
-            }
-            role = UserRoleEnum.ADMIN;
-        }
 
         // 사용자 저장
         User user = new User(username, password, requestDto.getName(), email, role);
