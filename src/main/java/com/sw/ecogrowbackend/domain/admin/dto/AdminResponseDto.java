af244c83a947fc5b47a3f300a98a8f685e79fffd
gminnimk <tvt1245@naver.com>
2024-10-08 17:09:46 +0900

    fix : name 필드 삭제에 따른 admin 관련 코드 수정

--- src/main/java/com/sw/ecogrowbackend/domain/admin/dto/AdminResponseDto.java
+++ src/main/java/com/sw/ecogrowbackend/domain/admin/dto/AdminResponseDto.java
@@ -2,4 +2,4 @@ package com.sw.ecogrowbackend.domain.admin.dto;
 
 import java.time.LocalDateTime;
 
-public record AdminResponseDto(Long id, String username, String name, String email, LocalDateTime createdAt, LocalDateTime modifiedAt, String role) {}
+public record AdminResponseDto(Long id, String username, String email, LocalDateTime createdAt, LocalDateTime modifiedAt, String role) {}
