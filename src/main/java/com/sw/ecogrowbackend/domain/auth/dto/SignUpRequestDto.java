f28fce5f31e711645888af3aa22781c3b06ec7d0
gminnimk <tvt1245@naver.com>
2024-10-08 17:06:03 +0900

    refactor : 빌터 패턴에서 NoArgsConstructor 사용으로 대체

--- src/main/java/com/sw/ecogrowbackend/domain/auth/dto/SignUpRequestDto.java
+++ src/main/java/com/sw/ecogrowbackend/domain/auth/dto/SignUpRequestDto.java
@@ -3,11 +3,11 @@ package com.sw.ecogrowbackend.domain.auth.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class SignUpRequestDto {
 
     @NotBlank(message = "아이디는 필수 사항입니다.")
@@ -19,11 +19,6 @@ public class SignUpRequestDto {
     @Size(min = 6, max = 255, message = "비밀번호는 최소 6글자 이상, 255글자 이하이어야 합니다.")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_])\\S{6,}$", message = "비밀번호는 알파벳 대소문자 + 숫자 + 특수문자로만 구성되어야 합니다.")
     private String password;
-
-    @NotBlank(message = "이름은 필수 사항입니다.")
-    @Size(min = 2, max = 20, message = "이름은 최소 2글자에서 20글자까지 입력 가능합니다.")
-    @Pattern(regexp = "^[a-zA-Z가-힣]+$", message = "한글 혹은 영어 입력만 가능합니다.")
-    private String name;
 
     @NotBlank(message = "이메일은 필수 사항입니다.")
     @Size(min = 2, max = 20, message = "이메일은 최소 2글자에서 20글자까지 입력 가능합니다.")
