2bc8bcb7386aceb51be473344f1ad0d334567367
gminnimk <tvt1245@naver.com>
2024-10-25 19:06:45 +0900

    fix : 생성자 충돌에 따른 Builder 삭제

--- src/main/java/com/sw/ecogrowbackend/domain/waste/dto/WasteRecordResponseDto.java
+++ src/main/java/com/sw/ecogrowbackend/domain/waste/dto/WasteRecordResponseDto.java
@@ -1,11 +1,9 @@
 package com.sw.ecogrowbackend.domain.waste.dto;
 
 import com.sw.ecogrowbackend.domain.waste.entity.WasteRecord;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class WasteRecordResponseDto {
 
     private Long id;
@@ -21,10 +19,6 @@ public class WasteRecordResponseDto {
 
     // 엔티티를 Dto로 변환하는 메서드
     public static WasteRecordResponseDto fromEntity(WasteRecord wasteRecord) {
-        return WasteRecordResponseDto.builder()
-            .id(wasteRecord.getId())
-            .wasteType(wasteRecord.getWasteType())
-            .amount(wasteRecord.getAmount())
-            .build();
+        return new WasteRecordResponseDto(wasteRecord);
     }
 }
