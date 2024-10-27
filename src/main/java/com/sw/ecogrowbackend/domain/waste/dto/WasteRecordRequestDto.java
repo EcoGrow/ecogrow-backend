1a525550c28355df793a0bb09a265ec6fd3caad8
gminnimk <tvt1245@naver.com>
2024-10-27 20:01:33 +0900

    fix : 리스트를 사용해 하나에 게시물에 다중 쓰레기 기록을 응답/요청 할 수 있게 설정

--- src/main/java/com/sw/ecogrowbackend/domain/waste/dto/WasteRecordRequestDto.java
+++ src/main/java/com/sw/ecogrowbackend/domain/waste/dto/WasteRecordRequestDto.java
@@ -1,21 +1,14 @@
 package com.sw.ecogrowbackend.domain.waste.dto;
 
+import jakarta.validation.Valid;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor
 public class WasteRecordRequestDto {
 
-    @NotBlank(message = "쓰레기 종류는 비워둘 수 없습니다.")
-    private String wasteType;
-
-    @Min(value = 0, message = "양은 0보다 커야 하며, 킬로그램(kg) 단위로 입력해야 합니다.")
-    private double amount;
-
-    @NotBlank(message = "단위는 비워둘 수 없습니다. 'kg' 또는 'g'를 사용하세요.")
-    private String unit;
+    @Valid
+    private List<WasteItemRequestDto> wasteItems;
 }
--- src/main/java/com/sw/ecogrowbackend/domain/waste/dto/WasteRecordResponseDto.java
+++ src/main/java/com/sw/ecogrowbackend/domain/waste/dto/WasteRecordResponseDto.java
@@ -1,20 +1,22 @@
 package com.sw.ecogrowbackend.domain.waste.dto;
 
 import com.sw.ecogrowbackend.domain.waste.entity.WasteRecord;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Getter;
 
 @Getter
 public class WasteRecordResponseDto {
 
     private Long id;
-    private String wasteType;
-    private double amount;
+    private List<WasteItemResponseDto> wasteItems;
 
     // WasteRecord를 인수로 받아 처리하는 생성자
     public WasteRecordResponseDto(WasteRecord wasteRecord) {
         this.id = wasteRecord.getId();
-        this.wasteType = wasteRecord.getWasteType();
-        this.amount = wasteRecord.getAmount();
+        this.wasteItems = wasteRecord.getWasteItems().stream()
+            .map(WasteItemResponseDto::new)
+            .collect(Collectors.toList());
     }
 
     // 엔티티를 Dto로 변환하는 메서드
