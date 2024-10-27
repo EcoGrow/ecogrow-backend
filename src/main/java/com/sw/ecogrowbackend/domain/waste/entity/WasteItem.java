1e96155c09a8a24bf49eead6ec561ec4149a4bde
gminnimk <tvt1245@naver.com>
2024-10-27 19:56:10 +0900

    feat : 하나의 쓰레기 기록 게시물에 여러 종류의 기록을 남길 수 있도록 WasteItem 엔티티 추가.

---
+++ src/main/java/com/sw/ecogrowbackend/domain/waste/entity/WasteItem.java
@@ -0,0 +1,40 @@
+package com.sw.ecogrowbackend.domain.waste.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class WasteItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false)
+    private String wasteType; // 쓰레기 종류 (ex: 플라스틱, 음식물 쓰레기 등)
+
+    @Setter
+    @Column(nullable = false)
+    private double amount; // 쓰레기 양 (예: kg 단위)
+
+    @Setter
+    @Column(nullable = false)
+    private String unit; // 단위
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "waste_record_id")
+    private WasteRecord wasteRecord;
+}
