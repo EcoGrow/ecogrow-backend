fb51a4a7973c709d951627660ad1283fcd052543
gminnimk <tvt1245@naver.com>
2024-10-27 19:58:52 +0900

    feat : WasteItem 엔티티와 일대다 관계 설정 & @JsonIgnoreProperties 이용한 양방향 무한 순환 참조 해결

--- src/main/java/com/sw/ecogrowbackend/domain/waste/entity/WasteRecord.java
+++ src/main/java/com/sw/ecogrowbackend/domain/waste/entity/WasteRecord.java
@@ -1,13 +1,14 @@
 package com.sw.ecogrowbackend.domain.waste.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sw.ecogrowbackend.common.Timestamped;
 import com.sw.ecogrowbackend.domain.auth.entity.User;
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import lombok.Setter;
 
 @Entity
 @Getter
@@ -19,31 +20,21 @@ public class WasteRecord extends Timestamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter
-    @Column(nullable = false)
-    private String wasteType; // 쓰레기 종류 (ex: 플라스틱, 음식물 쓰레기 등)
-
-    @Column(nullable = false)
-    private double amount; // 쓰레기 양 (예: kg 단위)
-
-    @Setter
-    @Column(nullable = false)
-    private String unit; // 단위
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     @JsonIgnore
     private User user;
 
-    // 기본 생성자
-    public WasteRecord(String wasteType, double amount, String unit, User user) {
-        this.wasteType = wasteType;
-        this.amount = amount;
-        this.unit = unit;
+    @OneToMany(mappedBy = "wasteRecord", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties("wasteRecord")
+    private List<WasteItem> wasteItems = new ArrayList<>(); // 여러 쓰레기 항목을 저장할 수 있도록 설정
+
+    public WasteRecord(User user) {
         this.user = user;
     }
 
-    public void setAmount(Double amount) {
-        this.amount = amount;
+    public void addWasteItem(WasteItem item) {
+        wasteItems.add(item);
+        item.setWasteRecord(this);
     }
 }
