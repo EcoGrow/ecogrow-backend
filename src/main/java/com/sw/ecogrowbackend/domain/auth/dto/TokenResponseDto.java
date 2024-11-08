312bded8080582d9b3b5d9229bd71e7a7769e8fc
gminnimk <tvt1245@naver.com>
2024-11-08 22:14:06 +0900

    feat : 로그인 후 'userId'가 'localStorage'에 저장되지 않는 문제 해결하기 위해 TokenResponseDto에 userId 필드 추가

--- src/main/java/com/sw/ecogrowbackend/domain/auth/dto/TokenResponseDto.java
+++ src/main/java/com/sw/ecogrowbackend/domain/auth/dto/TokenResponseDto.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 
 @Getter
 public class TokenResponseDto {
+    private Long userId;
     private String accessToken;
     private String refreshToken;
 
-    public TokenResponseDto(String accessToken, String refreshToken){
+    public TokenResponseDto(Long userId, String accessToken, String refreshToken) {
+        this.userId = userId;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
     }
