25f49917775230cb239c52254f970546f71796e5
gminnimk <tvt1245@naver.com>
2024-11-13 20:56:41 +0900

    feat : 제품 엔티티 생성

---
+++ src/main/java/com/sw/ecogrowbackend/domain/product/Product.java
@@ -0,0 +1,28 @@
+package com.sw.ecogrowbackend.domain.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String category;
+
+    private double price;
+
+    private String url;
+}
