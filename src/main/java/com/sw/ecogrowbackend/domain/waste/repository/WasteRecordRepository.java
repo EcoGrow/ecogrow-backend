59971bd4861e4f2933afcfa93d1561002e2bc0bc
gminnimk <tvt1245@naver.com>
2024-11-16 21:17:20 +0900

    refactor : 사용하지 않는 메서드 삭제

--- src/main/java/com/sw/ecogrowbackend/domain/waste/repository/WasteRecordRepository.java
+++ src/main/java/com/sw/ecogrowbackend/domain/waste/repository/WasteRecordRepository.java
@@ -4,15 +4,9 @@ import com.sw.ecogrowbackend.domain.auth.entity.User;
 import com.sw.ecogrowbackend.domain.waste.entity.WasteRecord;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WasteRecordRepository extends JpaRepository<WasteRecord, Long>, WasteRecordRepositoryCustom {
-
-    List<WasteRecord> findAllByUser(User user);
-
-    Page<WasteRecord> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
     // 특정 사용자의 특정 id를 가진 단일 WasteRecord 조회.
     Optional<WasteRecord> findByIdAndUser(Long id, User user);
