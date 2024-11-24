4537e57fb945c271566c267bf1d80b5c0b2b2f40
gminnimk <tvt1245@naver.com>
2024-11-24 21:12:18 +0900

    fix : CI with test DB

--- src/test/java/com/sw/ecogrowbackend/EcogrowBackendApplicationTests.java
+++ src/test/java/com/sw/ecogrowbackend/EcogrowBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.sw.ecogrowbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EcogrowBackendApplicationTests {
 
     @Test
