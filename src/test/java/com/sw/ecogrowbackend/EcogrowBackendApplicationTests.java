a980498917614d1b22e198fb0112d497c01b218d
gminnimk <tvt1245@naver.com>
2024-11-26 18:51:42 +0900

    refactor : 사용하지 않는 어노테이션 삭제

--- src/test/java/com/sw/ecogrowbackend/EcogrowBackendApplicationTests.java
+++ src/test/java/com/sw/ecogrowbackend/EcogrowBackendApplicationTests.java
@@ -2,10 +2,8 @@ package com.sw.ecogrowbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class EcogrowBackendApplicationTests {
 
     @Test
